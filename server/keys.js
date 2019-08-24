61a5b7abccb737a9a5b008f2e52cdb57f2eaa4ab
tomaszmucha <tmucha82@gmail.com>
2019-08-24 20:18:48 +0200

    init commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgPassword: process.env.PGPASSWORD,
+  pgHost: process.env.PGHOST,
+  pgPort: process.env.PGPORT,
+  pgDatabase: process.env.PGDATABASE,
+};
